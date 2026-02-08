a79f58d8d9bdf9806666584a7127afb22caf40b1
joeshangout <jonahmorandev@gmail.com>
2026-02-08 12:28:40 -0600

    Create Feed.tsx component in src/components/home/ directory

---
+++ src/components/home/Feed.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Feed: React.FC = () => {
+    return (
+        <div>
+            <h1>Feed Component</h1>
+            {/* Add your feed functionality here */}
+        </div>
+    );
+};
+
+export default Feed;
